3fad239b569b1cde521d8e21c15c515ba3202c93
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2016-04-04 12:21:50 -0300

    #63

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -4,79 +4,80 @@ namespace AppBundle\Menu;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
-class Builder extends ContainerAware
-{
-    public function mainMenu(FactoryInterface $factory, array $options)
-    {
-        $menu = $factory->createItem(
-            'root',
-            array(
-                'childrenAttributes' => array(
-                    'class' => 'sidebar-menu',
-                ),
-            )
-        );
+class Builder extends ContainerAware {
+	public function mainMenu( FactoryInterface $factory, array $options ) {
+		$menu = $factory->createItem(
+			'root',
+			array(
+				'childrenAttributes' => array(
+					'class' => 'sidebar-menu',
+				),
+			)
+		);
 
-        $menu->addChild(
-            'MENU PRINCIPAL'
-        )->setAttribute('class', 'header');
+		$menu->addChild(
+			'MENU PRINCIPAL'
+		)->setAttribute( 'class', 'header' );
 
 
 //        $userManager = $this->container->get('fos_user.user_manager');
-        $usuario = $this->container->get('security.token_storage')->getToken()->getUser();
+		$usuario = $this->container->get( 'security.token_storage' )->getToken()->getUser();
 
-        $em = $this->container->get('doctrine.orm.entity_manager');
+		$em = $this->container->get( 'doctrine.orm.entity_manager' );
 
-        $usuarioGrupos = $em->getRepository(
-            'UsuariosBundle:UsuarioGrupo'
-        )->findByUsuario($usuario);
+		$usuarioGrupos = $em->getRepository(
+			'UsuariosBundle:UsuarioGrupo'
+		)->findByUsuario( $usuario );
 
-        $grupo = null;
-        $permisosApp = array();
-        $aplicativo = array();
+		$grupos      = array();
+		$permisosApp = array();
+		$aplicativo  = array();
 
-        foreach ($usuarioGrupos as $usuarioGrupo) {
-            $grupo = $usuarioGrupo->getGrupo();
-        }
+		foreach ( $usuarioGrupos as $usuarioGrupo ) {
+			$grupos[] = $usuarioGrupo->getGrupo();
+		}
 
-        if ($grupo) {
-            $permisosApp = $grupo->getPermisoAplicacion();
-        }
-        $i = 0;
-        foreach ( $permisosApp as $permisoApp ) {
-            if ( $permisoApp->getActivo() ) {
-                $aplicativo[ $permisoApp->getItemAplicativo()->getAplicativo()->getNombre() ]['icono'] =
-                    $permisoApp->getItemAplicativo()->getAplicativo()->getIcono();
+		foreach ( $grupos as $grupo ) {
 
-                $aplicativo[ $permisoApp->getItemAplicativo()->getAplicativo()->getNombre() ]['hijos'][ $i ]['hijo'] = $permisoApp->getItemAplicativo()->getNombre();
-                $aplicativo[ $permisoApp->getItemAplicativo()->getAplicativo()->getNombre() ]['hijos'][ $i ]['ruta'] = $permisoApp->getItemAplicativo()->getRuta();
-                $i ++;
-            }
-        }
+			$permisosApp[] = $grupo->getPermisoAplicacion();
+		}
+		$i = 0;
+		foreach ( $permisosApp as $key => $item ) {
+			foreach ( $item as $permisoApp ) {
+				if ( $permisoApp->getActivo() ) {
+					$aplicativo[ $permisoApp->getItemAplicativo()->getAplicativo()->getNombre() ]['icono'] =
+						$permisoApp->getItemAplicativo()->getAplicativo()->getIcono();
 
-        foreach ($aplicativo as $key => $value) {
-            $menu->addChild(
-                $key,
-                array(
-                    'childrenAttributes' => array(
-                        'class' => 'treeview-menu',
-                    ),
-                )
-            )
-                ->setUri('#')
-                ->setExtra('icon', $value['icono'])
-                ->setAttribute('class', 'treeview');
-            foreach ($value['hijos'] as $indice => $valor) {
-                $menu[$key]
-                    ->addChild(
-                        $valor['hijo'],
-                        array(
-                            'route' => $valor['ruta'],
-                        )
-                    );
-            }
+					$aplicativo[ $permisoApp->getItemAplicativo()->getAplicativo()->getNombre() ]['hijos'][ $i ]['hijo'] = $permisoApp->getItemAplicativo()->getNombre();
+					$aplicativo[ $permisoApp->getItemAplicativo()->getAplicativo()->getNombre() ]['hijos'][ $i ]['ruta'] = $permisoApp->getItemAplicativo()->getRuta();
+					$i ++;
+				}
+			}
+		}
 
-        }
+		foreach ( $aplicativo as $key => $value ) {
+			$menu->addChild(
+				$key,
+				array(
+					'childrenAttributes' => array(
+						'class' => 'treeview-menu',
+					),
+				)
+			)
+			     ->setUri( '#' )
+			     ->setExtra( 'icon', $value['icono'] )
+			     ->setAttribute( 'class', 'treeview' );
+			foreach ( $value['hijos'] as $indice => $valor ) {
+				$menu[ $key ]
+					->addChild(
+						$valor['hijo'],
+						array(
+							'route' => $valor['ruta'],
+						)
+					);
+			}
+
+		}
 
 
 //administracion
@@ -134,78 +135,78 @@ class Builder extends ContainerAware
 //            );
 //
 //
-        if ($usuario->hasRole('ROLE_ADMIN')) {
-            //administracion
-            $menu->addChild(
-                'Administracion',
-                array(
-                    'childrenAttributes' => array(
-                        'class' => 'treeview-menu',
-                    ),
-                )
-            )
-                ->setUri('#')
-                ->setExtra('icon', 'fa fa-dashboard')
-                ->setAttribute('class', 'treeview');
+		if ( $usuario->hasRole( 'ROLE_ADMIN' ) ) {
+			//administracion
+			$menu->addChild(
+				'Administracion',
+				array(
+					'childrenAttributes' => array(
+						'class' => 'treeview-menu',
+					),
+				)
+			)
+			     ->setUri( '#' )
+			     ->setExtra( 'icon', 'fa fa-dashboard' )
+			     ->setAttribute( 'class', 'treeview' );
 
-            $menu['Administracion']
-                ->addChild(
-                    'Aplicativos',
-                    array(
-                        'route' => 'aplicativo',
-                    )
-                );
-            $menu['Administracion']
-                ->addChild(
-                    'Item Aplicativos',
-                    array(
-                        'route' => 'item_aplicativo',
-                    )
-                );
+			$menu['Administracion']
+				->addChild(
+					'Aplicativos',
+					array(
+						'route' => 'aplicativo',
+					)
+				);
+			$menu['Administracion']
+				->addChild(
+					'Item Aplicativos',
+					array(
+						'route' => 'item_aplicativo',
+					)
+				);
 
 //        Usuarios
-            $menu->addChild(
-                'Usuarios',
-                array(
-                    'childrenAttributes' => array(
-                        'class' => 'treeview-menu',
-                    ),
-                )
-            )
-                ->setUri('#')
-                ->setExtra('icon', 'fa fa-user')
-                ->setAttribute('class', 'treeview');
+			$menu->addChild(
+				'Usuarios',
+				array(
+					'childrenAttributes' => array(
+						'class' => 'treeview-menu',
+					),
+				)
+			)
+			     ->setUri( '#' )
+			     ->setExtra( 'icon', 'fa fa-user' )
+			     ->setAttribute( 'class', 'treeview' );
 
-            $menu['Usuarios']
-                ->addChild(
-                    'Listado',
-                    array(
-                        'route' => 'usuario',
-                    )
-                );
-            $menu['Usuarios']
-                ->addChild(
-                    'Grupos',
-                    array(
-                        'route' => 'fos_user_group_list',
-                    )
-                );
-            $menu['Usuarios']
-                ->addChild(
-                    'Permisos',
-                    array(
-                        'route' => 'permiso_aplicacion',
-                    )
-                );
-            $menu['Usuarios']
-                ->addChild(
-                    'Permisos Especiales',
-                    array(
-                        'route' => 'permiso_especial',
-                    )
-                );
-        }
+			$menu['Usuarios']
+				->addChild(
+					'Listado',
+					array(
+						'route' => 'usuario',
+					)
+				);
+			$menu['Usuarios']
+				->addChild(
+					'Grupos',
+					array(
+						'route' => 'fos_user_group_list',
+					)
+				);
+			$menu['Usuarios']
+				->addChild(
+					'Permisos',
+					array(
+						'route' => 'permiso_aplicacion',
+					)
+				);
+			$menu['Usuarios']
+				->addChild(
+					'Permisos Especiales',
+					array(
+						'route' => 'permiso_especial',
+					)
+				);
+		}
 
-        return $menu;
-    }
+		return $menu;
+	}
 }
